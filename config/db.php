b064d6975944a8efe5c65cf5137fbef762670ce7
hueiyuanSu <max19950618@gmail.com>
2018-01-23 11:25:52 +0800

    修改診所後台且移除無關聯的程式

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=docker.for.mac.localhost;dbname=seda',
+    'dsn' => 'mysql:host=docker.for.mac.localhost;dbname=clinic',
     'username' => 'max',
     'password' => '12345678',
     'charset' => 'utf8',
